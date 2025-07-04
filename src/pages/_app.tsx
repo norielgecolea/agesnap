4a53957123b2744f2361e6f69e1a46561049282c
softgenai <admin@softgen.ai>
2025-07-04 10:09:57 +0000

    Edit src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
-
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    &lt;ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange&gt;
-      &lt;Component {...pageProps} /&gt;
-    &lt;/ThemeProvider&gt;
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
